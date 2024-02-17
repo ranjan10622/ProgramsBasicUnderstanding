1e4eb517de54d72d572b7f04e04195fd18a2df10
Ranjan <ranjan22mohan@gmail.com>
2024-02-17 10:03:23 +0530

    this is my first commit

---
+++ src/abc/MergeTwoChar.java
@@ -0,0 +1,25 @@
+package abc;
+
+import java.util.Scanner;
+
+public class MergeTwoChar {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		 System.out.println("Enter your first name ");
+		     
+		    String fname= sc.nextLine();
+		       
+		    System.out.println("Enter your last name");
+		    
+		       String lname = sc.nextLine();
+		       
+		       System.out.println("Your name is "+ fname + lname);
+		       
+		    
+
+	}
+
+}
